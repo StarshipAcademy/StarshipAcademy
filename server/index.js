20e15351fe2ac86eaa8693e2e3eb037a4bede611
Eliot <eliotpszw@gmail.com>
2016-12-12 13:22:06 -0500

    Fixed up my console.logs

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 
 const app = require('express')();
 const _db = require('./db/db');
+const chalk = require('chalk');
 
 require('./configure')(app, _db);
 require('./sessions')(app);
@@ -9,7 +10,9 @@ require('./sessions')(app);
 app.use('/api', require('./routes'));
 
 app.get('/*', (req, res) => {
-  console.log(req.session);
+  if (req.session.socketData) {
+    console.log(chalk.magenta(`A user @ ${req.session.socketData.address} just visited the site.`));
+  }
   res.sendFile(app.getValue('indexPath'));
 });
 
