bfc77e2faad6b7492abcab30fbb67fb4659806c8
Daniel Kang <dkinsk@gmail.com>
2017-01-28 12:55:22 -0500

    added assets and models

---
+++ browser/src/components/Fence.js
@@ -0,0 +1,61 @@
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+AFRAME.registerComponent('fence', {
+  schema: {
+      width: {
+          type: 'number',
+          default: 10
+      },
+      depth: {
+          type: 'number',
+          default: 10
+      },
+      x0: {
+          type: 'number',
+          default: 0
+      },
+      y0: {
+          type: 'number',
+          default: 0
+      },
+      z0: {
+          type: 'number',
+          default: 0
+      }
+  },
+
+
+  tick: function() {
+      var data = this.data;
+      var thiswidth = data.width;
+      var thisdepth = data.depth;
+      var x0 = data.x0;
+      var y0 = data.y0;
+      var z0 = data.z0;
+
+      minX = thiswidth / 2 + x0;
+      maxX = ( -1 * thiswidth / 2 ) + x0;
+
+      minY = thiswidth / 2 + y0;
+      maxY = ( -1 * thiswidth / 2 ) + y0;
+
+      minZ = thisdepth / 2 + z0;
+      maxZ = ( -1 * thisdepth / 2 ) + z0;
+
+      var position = this.el.getComputedAttribute('position');
+
+      position.x = Math.min( minX, position.x);
+      position.x = Math.max( maxX, position.x);
+
+      position.y = Math.min( minY, position.y);
+      position.y = Math.max( maxY, position.y);
+
+      position.z = Math.min( minZ, position.z);
+      position.z = Math.max( maxZ, position.z);
+
+      this.el.setAttribute('position', position);
+  },
+
+});
--- browser/src/index.js
+++ browser/src/index.js
@@ -1,1 +1,12 @@
-index.js
+
+require('aframe');
+require('aframe-randomizer-components');
+require('aframe-extras');
+require('./components/onClickListener');
+require('./components/EntityGenerator');
+require('./components/Projectile');
+require('./components/BulletSpawner');
+require('./components/Sky');
+require('./components/Text');
+require('./components/Camera');
+require('./components/Fence');
