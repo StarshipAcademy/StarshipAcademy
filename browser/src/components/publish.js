77191cadddace76a03e9f943b194853c0d3c82bc
Charity Corcino <cecicorcino@gmail.com>
2017-01-30 15:06:19 -0500

    closes #14

---
+++ browser/src/components/publish.js
@@ -0,0 +1,21 @@
+import AFRAME from 'aframe';
+
+let hasGottenOthers = false;
+
+export default AFRAME.registerComponent('publish-location', {
+  tick: function () {
+    socket.on('startTick', () => hasGottenOthers = true);
+    if (hasGottenOthers) {
+      const el = this.el;
+      socket.emit('tick', {
+        id: el.getAttribute('id'),
+        x: el.getAttribute('position').x,
+        y: el.getAttribute('position').y,
+        z: el.getAttribute('position').z,
+        xrot: el.getAttribute('rotation').x,
+        yrot: el.getAttribute('rotation').y,
+        zrot: el.getAttribute('rotation').z
+      });
+    }
+  }
+});
---
+++ browser/utils.js
@@ -0,0 +1,18 @@
+export function putUserOnDOM (user) {
+  const scene = document.getElementById('scene');
+  const avatar = document.createElement('a-entity');
+  scene.appendChild(avatar);
+  avatar.setAttribute('id', user.id);
+  avatar.setAttribute('geometry', 'primitive', 'box');
+  avatar.setAttribute('material', 'color', user.color);
+  avatar.setAttribute('position', `${user.x} ${user.y} ${user.z}`);
+  avatar.setAttribute('rotation', `${user.xrot} ${user.yrot} ${user.zrot}`);
+  return avatar;
+}
+
+export function addFirstPersonProperties (avatar) {
+  avatar.setAttribute('publish-location', true);
+  avatar.setAttribute('camera', true);
+  avatar.setAttribute('look-controls', true);
+  avatar.setAttribute('wasd-controls', true);
+}
