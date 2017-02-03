7ad2a2bc9d4fd302b4cd4dd4d12854ac1727ee91
Charity Corcino <cecicorcino@gmail.com>
2017-02-03 15:36:55 -0500

    closes #18

--- browser/src/components/Appload.js
+++ browser/src/components/Appload.js
@@ -7,6 +7,7 @@ AFRAME.registerComponent('click-list', {
         '<a-asset-item id="arc170-mtl" src="./src/assets/models/arc170/arc170.mtl"></a-asset-item> ' + '<a-mixin id="asteroid1" geometry="primitive: sphere; radius:2" material="src: #asteroid"></a-mixin>' +
         '<a-mixin id="asteroid2" geometry="primitive: sphere; radius:4" material="src: #asteroid"></a-mixin>' +'<a-mixin id="position" random-spherical-position="radius: 40; startX: 0; lengthX: 360; startY: 0; lengthY: 360" random-rotation random-position="min: -100 -100 -100; max: 100 100 100"></a-mixin>'+
  '<a-mixin id="laser" '+ 'geometry="primitive: cylinder; radius: 0.1; height: 8 "' + 'rotation="-90 0 0"' + 'translate="0 -2 0" ' + 'material="color: yellow; metalness: 0.2; opacity: 0.8; roughness: 0.3" ' + 'projectile="speed: 0.02 "' + 'sound="src: #laser_shot; on: click; autoplay: true; volume: 1"></a-mixin> ' + '</a-asset>' + '<a-entity entity-generator="mixin: asteroid1 position; num: 150;"></a-entity>' + '<a-entity entity-generator="mixin: asteroid2 position; num: 150;"></a-entity>' +'<a-sky src="./src/assets/images/space.jpg"></a-sky> ' + '</a-scene> ' + '</body>';
+ 
  $('#menu').replaceWith(newScene);
 
       el.emit('click', null, false);
