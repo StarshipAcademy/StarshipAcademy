47a6c9ea6bebf54a56fde17b045df200c1d106a9
Jimmy <jdicolandrea@gmail.com>
2017-02-07 15:28:10 -0500

    bring turrets back online

--- browser/body.js
+++ browser/body.js
@@ -26,8 +26,8 @@ var body = `
         <a-asset-item id="arc170-mtl" src="./src/assets/models/arc170/arc170.mtl"></a-asset-item>
 
         <!-- TURRET -->
-        <!-- <a-asset-item id="turretBot-obj" src="./src/assets/models/turretBot/turretBot.obj"></a-asset-item>
-        <a-asset-item id="turretBot-mtl" src="./src/assets/models/turretBot/turretBot.mtl"></a-asset-item> -->
+        <a-asset-item id="turretBot-obj" src="./src/assets/models/turretBot/turretBot.obj"></a-asset-item>
+        <a-asset-item id="turretBot-mtl" src="./src/assets/models/turretBot/turretBot.mtl"></a-asset-item>
 
         <a-mixin id="asteroid1" geometry="primitive: sphere; radius:2" material="src: #asteroid"></a-mixin>
         <a-mixin id="asteroid2" geometry="primitive: sphere; radius:4" material="src: #asteroid"></a-mixin>
